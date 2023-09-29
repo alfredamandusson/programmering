160243d1f59beb46f860e4a3c6c664abe764b776
alfred amandusson <alfred.amandusson@skola.setnungsund.se>
2023-09-29 15:08:28 +0200

    while slingor

---
+++ 7060 while/uppgift_1.py
@@ -0,0 +1,34 @@
+'''A'''
+i = 3
+while i < 11:
+    print(i)
+    i = i + 1
+print("A klar")
+
+# B
+i = 2
+while i < 13:
+    print(i)
+    i = i + 2
+print("B klar")
+
+# C
+i = 10
+while i > -1:
+    print(i)
+    i = i - 1
+print("C klar")
+
+#D 
+i = 11
+while i > 1:
+    print(i)
+    i = i - 3
+print("D klar")
+
+# E
+i = 1
+while i < 6:
+    print(i**2)
+    i = (i +1)
+print("D klar")
