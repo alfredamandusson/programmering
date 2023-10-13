64da8a544fa3b10089115f016aaaaaf7764b07dc
alfred amandusson <alfred.amandusson@skola.setnungsund.se>
2023-10-13 15:29:09 +0200

    klar

---
+++ 7060 while/uppgift_2.py
@@ -0,0 +1,8 @@
+antal = int(input("Hur många heltal vill du ha? "))
+minsta=int(input("vilket är minsta talet i serien"))
+
+i = minsta
+while i < antal + minsta:
+    print(i)
+    i = i + 1
+print("A klar")
