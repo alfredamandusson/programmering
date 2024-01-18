fa874a5de047bb32fca951b6d2566e89d9e76896
alfred amandusson <alfred.amandusson@skola.setnungsund.se>
2024-01-18 09:15:41 +0100

    klar

---
+++ 7073 flödesdiagram/7095 for slingor med range/7096 for each slinga/7100 listor/uppgift_1.py
@@ -0,0 +1,6 @@
+sexsidigtärning= [1,3,3,5,6,5,5,2,1,4]
+print("min: ",min(sexsidigtärning))
+sexsidigtärning.sort()
+print("sorterad: ", sexsidigtärning)
+#sorterad: 
+sum("sexsidigtärning") 
--- 7073 flödesdiagram/7095 for slingor med range/7096 for each slinga/uppgift_2.py
+++ 7073 flödesdiagram/7095 for slingor med range/7096 for each slinga/uppgift_2.py
@@ -1,6 +1,7 @@
-if bokstav == '13324343532611465515256554532451154255241636666454':
- antal6=0       
- for bokstav in ord:
+ord = '13324343532611465515256554532451154255241636666454'
+antal6=0       
+for bokstav in ord:
     print(bokstav)
     if bokstav == '6':
- antal6 = antal6 
+       antal6 = antal6 +1
+print(antal6, "sexor")
