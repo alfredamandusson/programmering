8d55141e0530a2bb8a035436ba276780a61707d5
alfred amandusson <alfred.amandusson@skola.setnungsund.se>
2024-02-09 16:03:49 +0100

    på god väg

--- 7073 flödesdiagram/7080 definera funktioner/7110 dictonary/projekt sten sax pöse/projekt sten sax påse.py
+++ 7073 flödesdiagram/7080 definera funktioner/7110 dictonary/projekt sten sax pöse/projekt sten sax påse.py
@@ -1,6 +1,10 @@
 import random
 
 def get_user_choise():
-    while true:
+    while True:
         user_choise=input("välj sten, sax eller påse")
-   
+        if user_choise in ['sten', 'sax', 'påse']:
+            return user_choise
+else:
+    print("ogiltigt val. Var god välj sten, sax eller påse")  
+        
